dd969d63a3c3872be4d397bfa4435c7f228e5139
Ra1nWarden <wzh19921016@gmail.com>
2015-11-21 11:24:30 -0500

    Finished REST Api

---
+++ node-api/server.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+var User = require('./app/models/user');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \
+  Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://node:node@apollo.modulusmongo.net:27017/beD9uqat');
+
+app.get('/', function(req, res) {
+  res.send('Welcome to the home page!');
+});
+
+var apiRouter = express.Router();
+
+apiRouter.use(function(req, res, next) {
+  console.log('Somebody just visited our app!');
+  next();
+});
+
+apiRouter.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!'});
+});
+
+apiRouter.route('/users')
+  .post(function(req, res) {
+    var user = new User();
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+
+    user.save(function(err) {
+      if(err) {
+        console.log('Error in saving!');
+        if(err.code == 11000)
+          return res.json({ success: false, message: 'A user with that username already exists. '});
+        else
+          return res.send(err);
+      } else {
+        console.log('Succeeded!');
+        return res.json( { message: 'User created!' });
+      }
+    })
+  })
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if(err)
+        res.send(err);
+      res.json(users);
+    });
+  });
+
+apiRouter.route('/users/:user_id')
+  .get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if(err)
+        res.send(err);
+      res.json(user);
+    });
+  })
+  .put(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if(err)
+        res.send(err);
+      if(req.body.name) {
+        user.name = req.body.name;
+      }
+      if(req.body.username) {
+        user.username = req.body.username;
+      }
+      if(req.body.password) {
+        user.password = req.body.password;
+      }
+      user.save(function(err) {
+        if(err)
+          res.send(err);
+        res.json({ message: 'User updated!' });
+      });
+    })
+  })
+  .delete(function(req, res) {
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user) {
+      if(err)
+        res.send(err);
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
+
+app.use('/api', apiRouter);
+
+app.listen(port);
+console.log('Listening on port ' + port);
